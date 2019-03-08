692d827fc01bc4a40d5ad5061e5d8d63e790122f
sekhar <sekhar@10.79.1.85>
2019-03-08 13:03:34 +0530

    update constant.java

--- src/main/java/com/dcx/config/Constants.java
+++ src/main/java/com/dcx/config/Constants.java
@@ -1,11 +1,20 @@
 package com.dcx.config;
 
+import java.io.File;
+
 public class Constants {
 	
-	public static final String PROPERTY_FILE_PATH = System.getProperty("user.dir")+"\\src\\main\\java\\com\\dcx\\config\\OS.properties";
+	/*public static final String PROPERTY_FILE_PATH = System.getProperty("user.dir")+"\\src\\main\\java\\com\\dcx\\config\\OS.properties";
 	public static final String EXCEL_FILE_PATH = System.getProperty("user.dir")+"\\Data\\Data.xlsx";
 	public static final String CONFIG_FILE_PATH = System.getProperty("user.dir")+"\\src\\main\\java\\com\\dcx\\config\\";
 	public static final String SCREENSHOT_FILE_PATH = System.getProperty("user.dir")+"\\ExtentReports\\Screenshots\\";
 	public static final String REPORTS_FILE_PATH = System.getProperty("user.dir")+"\\ExtentReports\\report.html";
-	public static final String REPORTS_CONFIG_FILE_PATH = System.getProperty("user.dir")+"\\ReportsConfig.xml";
+	public static final String REPORTS_CONFIG_FILE_PATH = System.getProperty("user.dir")+"\\ReportsConfig.xml";*/
+	
+	public static final String PROPERTY_FILE_PATH = System.getProperty("user.dir")+File.separator+"src"+File.separator+"main"+File.separator+"java"+File.separator+"com"+File.separator+"dcx"+File.separator+"config"+File.separator+"OS.properties";
+	public static final String EXCEL_FILE_PATH = System.getProperty("user.dir")+File.separator+"Data"+File.separator+"Data.xlsx";
+	public static final String CONFIG_FILE_PATH = System.getProperty("user.dir")+File.separator+"src"+File.separator+"main"+File.separator+"java"+File.separator+"com"+File.separator+"dcx"+File.separator+"config"+File.separator;
+	public static final String SCREENSHOT_FILE_PATH = System.getProperty("user.dir")+File.separator+"ExtentReports"+File.separator+"Screenshots"+File.separator;
+	public static final String REPORTS_FILE_PATH = System.getProperty("user.dir")+File.separator+"ExtentReports"+File.separator+"report.html";
+	public static final String REPORTS_CONFIG_FILE_PATH = System.getProperty("user.dir")+File.separator+"ReportsConfig.xml";
 }
